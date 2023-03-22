630a35a3dd7b476744e3c08fab5836b1395e8eea
Uchenna <uchennangwu21@gmail.com>
2023-03-22 16:03:39 +0100

    adding .circleci

---
+++ Eks/Backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    # Name of the bucket
+    bucket = "utchay-terraform"
+    key = "utchay/terraform/terraform.tfstate"
+    region = "us-east-1"
+
+
+     # DynamoDB table name
+    dynamodb_table = "terraform-up-locks"
+    encrypt = true
+  }
+}
