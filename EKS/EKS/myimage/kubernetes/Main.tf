b3efa8899857ca1266ed92ef894638045a931574
Uchenna <uchennangwu21@gmail.com>
2023-03-22 15:58:37 +0100

    rearranging for my pipeline

--- EKS/EKS/myimage/kubernetes/Main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "utchay-terraform"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-test-locks"
-  }
-}
